20cecaa6865ef8b9a12ebf31df0fbc84e47c2e2a
Dan Miller <danjm.com@gmail.com>
2020-10-09 18:59:07 -0230

    Temp commit to allow testing by aggId

--- ui/app/pages/swaps/select-quote-popover/sort-list/sort-list.js
+++ ui/app/pages/swaps/select-quote-popover/sort-list/sort-list.js
@@ -128,7 +128,7 @@ export default function SortList ({
                   'select-quote-popover__quote-source-label--blue': quoteSource === 'DEX',
                 })}
               >
-                {quoteSource}
+                {aggId}
               </div>
             </div>
             <div
